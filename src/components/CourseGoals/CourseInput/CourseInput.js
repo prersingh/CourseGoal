9601ec86f5732d5e786846711bcaba64099b48bf
preranasinghh <preranasingh1176@gmail.com>
2023-04-14 16:05:01 +0530

    react

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -29,7 +29,7 @@ const CourseInput = props => {
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button style={{color:!isVaild ? 'pink' : 'red'}} type="submit">Add Goal</Button>
+      <Button type="submit">Add Goal</Button>
     </form>
   );
 };
